fced8dbc17e08d0da89563a1747550192e2f3c4b
Ben Dunnell <bjdunnell96@gmail.com>
2019-09-22 19:34:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,8 +22,6 @@ def retrieval
 @@ -21,7 +24,7 @@ def retrieval
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
   shipping_manifest["oil paintings"]
 end
 
@@ -31,10 +29,8 @@ def adding
 @@ -31,13 +34,13 @@ def adding
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
 
   shipping_manifest["muskets"] = 2
-
 
 
   shipping_manifest["gun powder"] = 4
