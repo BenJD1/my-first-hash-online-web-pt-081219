4506a03bc189bb557977bcd1fde20511176ee9fd
Ben Dunnell <bjdunnell96@gmail.com>
2019-09-22 19:38:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,15 +1,8 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   {"name" => "Mitch"}
 
 end
-
-@@ -9,8 +9,11 @@ def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
 
   the_manifest = {
                   "whale bone corsets" => 5, 
@@ -19,14 +12,20 @@ end
 end
 
 def retrieval
-@@ -21,7 +24,7 @@ def retrieval
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
                  }
+
 
   shipping_manifest["oil paintings"]
 end
 
 def adding
-@@ -31,13 +34,13 @@ def adding
+  shipping_manifest = {
+                  "whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
 
